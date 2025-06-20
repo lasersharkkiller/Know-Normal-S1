9a10c56439626f408bc3a02cb46e1e238b1ae2d9
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2025-06-20 15:04:03 -0400

    Update ProcSignature.psm1

--- AlertsModules/ProcSignature.psm1
+++ AlertsModules/ProcSignature.psm1
@@ -6,13 +6,14 @@ function Get-ProcSigInfo{
         $timeToBaseline,
         $os,
         $currentTime,
-        $lastDayTime
+        $lastDayTime,
+        $apiToken
     )
 
 
 # Define variables
-$apiToken = ''
-$baseUrl = 'https://usea1-company.sentinelone.net/web/api/v2.1'
+$apiToken = $apiToken
+$baseUrl = 'https://site.sentinelone.net/web/api/v2.1'
 $query = "endpoint.os = '$os' and src.process.name = '$srcProcName' | columns src.process.publisher, src.process.signedStatus, src.process.verifiedStatus  | group srcProcCount = count (src.process.publisher) by src.process.publisher,src.process.signedStatus, src.process.verifiedStatus  | sort -srcProcCount | limit 100"
 #$siteId = 'your_site_id_here' # Replace with your actual Site ID (optional, depending on the scope of your query)
 $pollingInterval = 1 # Interval in seconds to check the status of the query

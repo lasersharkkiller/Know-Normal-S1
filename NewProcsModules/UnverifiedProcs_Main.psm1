3dee4c9147f26f6ea3f3910fa0e6eb07211bad0a
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2025-07-09 15:56:49 -0400

    Update UnverifiedProcs_Main.psm1

--- NewProcsModules/UnverifiedProcs_Main.psm1
+++ NewProcsModules/UnverifiedProcs_Main.psm1
@@ -5,10 +5,12 @@ Import-Module -Name ".\NewProcsModules\UnverifiedProcRecent.psm1"
 Import-Module -Name ".\NewProcsModules\Intezer_Analyze_By_Hash.psm1"
 Import-Module -Name ".\NewProcsModules\S1PullFile.psm1"
 Import-Module -Name ".\NewProcsModules\S1GetActivities.psm1"
+Import-Module -Name ".\NewProcsModules\S1GetActivities.psm1"
+Import-Module -Name ".\NewProcsModules\PullFromVT.psm1"
 
 # Define variables
-$apiToken = 'eyJraWQiOiJ1cy1lYXN0LTEtcHJvZC0wIiwiYWxnIjoiRVMyNTYifQ.eyJzdWIiOiJpYW4ubm9ydG9uQGVxdWlmYXguY29tIiwiaXNzIjoiYXV0aG4tdXMtZWFzdC0xLXByb2QiLCJkZXBsb3ltZW50X2lkIjoiNjc5NDgiLCJ0eXBlIjoidXNlciIsImV4cCI6MTc1MDUxMzA0NiwiaWF0IjoxNzQ3OTIxMDQ2LCJqdGkiOiJjZjQ3N2YzYy03ZWM1LTRkMmYtOTM0OC00NzVmZTA1NDRjZTgifQ.lFzgjVpAnajoQoRVNk5OVgAkwhNe765EsAT9g93TB4Ron6Z8bhFhx-uVQ9_ON5__WYitnML6sFodzuZx39R1Xw'
-$baseUrl = 'https://site.sentinelone.net/web/api/v2.1'
+$apiToken = ''
+$baseUrl = 'https://usea1-company.sentinelone.net/web/api/v2.1'
 $queryCreateUrl = "$baseUrl/dv/events/pq"
 
 $pollingInterval = 1 # Interval in seconds to check the status of the query
@@ -43,9 +45,16 @@ foreach ($newProc in $filteredUnverifiedProcsRecent){
     $fileName = $newProc.value[0]
     $newHash = $newProc.value[2]
     [bool]$pullFileFromS1 = $false
+    [bool]$pullFileFromVT = $false
 
+    #first check if it already exists in Intezer
     $pullFileFromS1 = Get-IntezerHash -checkHash $newHash -fileName $fileName -baseline "output\unverifiedProcsBaseline.json" -signatureStatus "unverified" -ErrorAction silentlycontinue
+    #if it's not in intezer, first try VT (before pulling with S1 - more efficient)
     if ($pullFileFromS1 -eq $false){
+        $pullFileFromVT = Get-PullFromVT -Sha256 $newHash -fileName $fileName -ErrorAction silentlycontinue
+    }
+    
+    if ($pullFileFromS1 -eq $false -and $pullFileFromVT -eq $false){
         $agentId = Get-FileFromS1 -headers $headers -baseUrl $baseUrl -queryCreateUrl $queryCreateUrl -pollingInterval $pollingInterval -queryDays $queryDays -newHash $newHash
     } else {
         continue

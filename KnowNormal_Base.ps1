41bb258b145aa169469dd614e45e9fbc5b21b13e
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2025-07-09 15:44:31 -0400

    Update KnowNormal_Base.ps1

--- KnowNormal_Base.ps1
+++ KnowNormal_Base.ps1
@@ -1,7 +1,8 @@
 Import-Module -Name ".\NewProcsModules\MispPull.psm1"
+Import-Module -Name ".\NewProcsModules\BlockedCountryPull.psm1"
 Import-Module -Name ".\AlertsModules\Alerts_Main.psm1"
 Import-Module -Name ".\NewProcsModules\UnverifiedProcs_Main.psm1"
-Import-Module -Name ".\NewProcsModules\UnsignedWinProcs_Main.psm1"
+Import-Module -Name ".\NewProcsModules\UnsignedProcs_Main.psm1"
 Import-Module -Name ".\stringsBaseline\BaseLineStrings_with_Intezer.psm1"
 Import-Module -Name ".\NewProcsModules\Intezer_Analyze_By_Hash.psm1"
 Import-Module -Name ".\NewProcsModules\IntezerCheckUrl.psm1"
@@ -9,12 +10,16 @@ Import-Module -Name ".\NewProcsModules\CheckAgainstVT.psm1"
 Import-Module -Name ".\NewProcsModules\CheckApiVoid.psm1"
 Import-Module -Name ".\NewProcsModules\GetASN-Cymru.psm1"
 Import-Module -Name ".\NewProcsModules\CheckThreatGrid.psm1"
+Import-Module -Name ".\nsm\S1PullIpsForProcess.psm1"
+Import-Module -Name ".\nsm\processBulkIps.psm1"
+Import-Module -Name ".\nsm\CheckDevoNetworkAttacks.psm1"
 
 #Get Updates from MISP
 try {
     Get-MispPull
+    Get-BlockedCountryList
 } catch {
-    Write-Host "Unable to download the latest internal block list"
+    Write-Host "Unable to download the latest internal block lists"
 }
 
 Write-Host "Choose which function you would like to use:"
@@ -23,6 +28,9 @@ Write-Host "2) New Unverified Processes"
 Write-Host "3) New Unsigned Windows Processes"
 Write-Host "4) New Unsigned Linux Processes"
 Write-Host "5) Baseline Strings from the Processes"
+Write-Host "6) Pull Outbound C2 for a Process and Cross Reference Reputation"
+Write-Host "7) Process Bulk Ips"
+Write-Host "8) Check IPS/WAF perimeter activity (for feeding sus ASNs)"
 
 $functionChoice = Read-Host "Enter an option"
     
@@ -33,13 +41,22 @@ $functionChoice = Read-Host "Enter an option"
         Get-UnverifiedProcs
     }
     elseif ($functionChoice -eq 3){
-        Get-UnsignedWinProcs
+        Get-UnsignedProcs -os "windows"
     }
     elseif ($functionChoice -eq 4){
-        Get-UnsignedLinuxProcs
+        Get-UnsignedProcs -os "linux"
     }
     elseif ($functionChoice -eq 5){
         Get-StringsBaseline
+    }
+    elseif ($functionChoice -eq 6){
+        Get-S1PullIpsForProcess
+    }
+    elseif ($functionChoice -eq 7){
+        Get-ProcessBulkIps
+    }
+    elseif ($functionChoice -eq 8){
+        Get-CheckDevoNetworkAttacks
     }
     else {
       Write-Host "You did not choose a valid option"

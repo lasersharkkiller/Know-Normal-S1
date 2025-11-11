573a76bd96a89fcd4b7e19d1a40b6e83a6184a48
lasersharkkiller <35998923+lasersharkkiller@users.noreply.github.com>
2025-11-11 10:09:44 -0800

    Update DnsReqsforAlertProc.psm1

--- AlertsModules/DnsReqsforAlertProc.psm1
+++ AlertsModules/DnsReqsforAlertProc.psm1
@@ -13,7 +13,7 @@ function Get-DnsReqsforAlertProc{
 
 # Define variables
 $apiToken = $apiToken
-$baseUrl = 'https://site.net/web/api/v2.1'
+$baseUrl = 'https://usea1-equifax.sentinelone.net/web/api/v2.1'
 $query = "endpoint.name = '$hostName' and src.process.name = '$srcProcName' | columns event.dns.request  | group DnsReqCount = count (event.dns.request) by event.dns.request  | sort -DnsReqCount | limit 1000"
 #$siteId = 'your_site_id_here' # Replace with your actual Site ID (optional, depending on the scope of your query)
 $pollingInterval = 1 # Interval in seconds to check the status of the query
